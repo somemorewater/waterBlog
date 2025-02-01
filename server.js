db11248debdb224054cd663d1f27dacab5933aeb
somemorewater <oboyiloejeh@gmail.com>
2025-02-01 10:37:51 +0100

    enhance HTTP server to serve multiple file types and update port to 3000

--- server.js
+++ server.js
@@ -3,19 +3,34 @@ const fs = require("fs");
 const path = require("path");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        res.end("Error loading page");
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      }
-    });
-  }
+  let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url);
+  let extname = path.extname(filePath);
+  let contentType = "text/html";
+
+  // Set correct content type for common file types
+  const mimeTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".gif": "image/gif",
+    ".svg": "image/svg+xml",
+  };
+
+  if (mimeTypes[extname]) contentType = mimeTypes[extname];
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    }
+  });
 });
 
-server.listen(1969, () => {
-  console.log("Server running at http://localhost:1969");
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
 });
